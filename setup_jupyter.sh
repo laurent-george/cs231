d56c1024966eef7e633cd21146709c5813f151e4
Laurent George <laurent.f.george@gmail.com>
2017-06-30 18:51:42 +0200

    add setup dark theme

---
+++ setup_jupyter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#source ~/anaconda3/bin/activate
+
+#jt -l -t onedork -fs 95 -altp -tfs 11 -nfs 115 -cellw 88%
+
+
+jt -l -t monokai  -fs 95 -vim -tfs 11 -nfs 115 -cellw 88% 
+#jt -l -t monokai  -fs 95 -tfs 11 -nfs 115 -cellw 88% 
+
